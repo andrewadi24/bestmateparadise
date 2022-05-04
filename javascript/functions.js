0ce89d9f65e30751c3f4a0ee3765be9d1bb33185
andrew adisaputra <aadi5934@uni.sydney.edu.au>
2022-05-04 14:42:02 +1000

    edit profile register new pet submit button fixed

--- javascript/functions.js
+++ javascript/functions.js
@@ -6,3 +6,13 @@ document.getElementById("hamburger1").addEventListener("click", function(){
 document.getElementById("close1").addEventListener("click", function(){
     document.getElementById("mobile-nav1").classList.add("hidden");
 })
+
+function myalert(){
+    alert("Sucessfully edited profile")
+}
+function myalert1(){
+    alert("Sucessfully registered new pet, it will take 1 day for us to process, and update the list")
+}
+function enquiries(){
+    alert("Thank you for your enquiry, our team will respond to your enquiry shortly")
+}
